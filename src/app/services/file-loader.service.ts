b02605a636fa4febc9acf6b341635a2aabe7c600
hpham <hpham@checkpoint.com>
2018-08-08 14:27:07 -0700

    Fix the menu conversion to retain the order of menu items.

--- src/app/services/file-loader.service.ts
+++ src/app/services/file-loader.service.ts
@@ -15,18 +15,17 @@ export class FileLoaderService {
       let fileReader = new FileReader();
       fileReader.readAsText(file);
       fileReader.onload = () => {
-        let cpview_conf = JSON.parse(xml2json(fileReader.result, {compact: true}));
-        let menus = cpview_conf.CPViewConfig.Menus; delete menus._attributes;
-        let views_arr = cpview_conf.CPViewConfig.Views.View;
+        let menu = JSON.parse(xml2json(fileReader.result, {compact: false})).elements[0];
+        let views_arr = JSON.parse(xml2json(fileReader.result, {compact: true})).CPViewConfig.Views.View;
+
+        // Convert menus into a dictionary with menu name as key recursively and flatten the views in a menu to only contain view name
+        menu = this._flatten_menu(menu);
 
         // Convert views into a dictionary with viewtag as key
         let views = {};
         for (let view of views_arr) {
           views[view._attributes.viewtag] = view;
         }
-
-        // Convert menus into a dictionary with viewtag or menu name as key recursively
-        let menu = this._flatten_menu(menus);
 
         observer.next([menu, views]);
       }
@@ -38,34 +37,14 @@ export class FileLoaderService {
     return this.cpview_conf$;
   }
 
-  private _flatten_menu(menus) {
+  private _flatten_menu(menu) {
     let flattened_menu = {};
-
-    // Process the child Menu
-    if ('Menu' in menus) {
-      let Menu = menus['Menu'];
-      if (Array.isArray(Menu)) {
-        for (let menu of Menu) {
-          flattened_menu[menu._attributes.name] = this._flatten_menu(menu);
-        }
-      } else {
-        flattened_menu[Menu._attributes.name] = this._flatten_menu(Menu);
+    for (let elem of menu.elements) {
+      if (elem.name == 'Menu' || elem.name == 'Menus') {
+        flattened_menu[elem.attributes.name] = this._flatten_menu(elem);
+      } else if (elem.name == 'View' || elem.name == 'DynamicView') {
+        flattened_menu[elem.attributes.viewtag] = 1;
       }
-    }
-
-    // Process the child Views
-    if ('View' in menus || 'DynamicView' in menus) {
-      let View = menus[('View' in menus) ? 'View' : 'DynamicView'];
-      let views = [];
-      if (Array.isArray(View)) {
-        for (let view of View) {
-          views.push(view._attributes.viewtag);
-        }
-      } else {
-        views.push(View._attributes.viewtag);
-      }
-      flattened_menu["Views"] = views;
-
     }
 
     return flattened_menu;
